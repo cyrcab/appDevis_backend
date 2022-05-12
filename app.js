d942173dc77f9898dadb07b4aa1fe78b2ba8010e
cyril cabrolier <cyr.cab@hotmail.fr>
2022-05-12 15:39:16 +0200

    initialisation server

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+const { SERVER_PORT } = process.env;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// server setup
+const server = app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
+});
+
+module.exports = server;
