e96fcc806363ff7881a330978beac6a90f704571
cyril cabrolier <cyr.cab@hotmail.fr>
2022-05-16 14:33:06 +0200

    update controller done

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,5 @@
 const prisma = require('../helpers/prismaClient.js');
+const formatDate = require('../helpers/formatDate');
 
 async function handleGetAllUsers(req, res, next) {
   try {
@@ -60,15 +61,58 @@ async function handleDeleteUser(req, res, next) {
           id: user.id,
         },
       });
-      res.status(200).json({ message: 'user correctly deleted', user: { ...user } });
+      res
+        .status(200)
+        .json({ message: 'user correctly deleted', user: { ...user }, isDeleted: true });
     } else {
       res.status(404).json({
-        message: 'user not found, did you enter the correct id',
+        message: 'user not found, did you enter the correct id ?',
+        idSearched: `${id}`,
+        isDeleted: false,
       });
     }
   } catch (error) {
     console.error(error);
     next(error);
+  }
+}
+async function handleUpdateUser(req, res, next) {
+  try {
+    const updateDate = formatDate(new Date());
+    const { id } = req.params;
+    const dataToUpdate = req.body;
+    const user = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (user) {
+      const updatedUser = await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          ...dataToUpdate,
+          updated_at: updateDate,
+        },
+      });
+      res.status(200).json({
+        message: 'user updated with success',
+        isModified: true,
+        lastDatas: { ...user },
+        dataUpdated: { ...dataToUpdate, updated_at: updateDate },
+        newDatas: { ...updatedUser },
+      });
+    } else {
+      res.status(404).json({
+        message: 'user not found, did you enter the correct id ?',
+        idSearched: `${id}`,
+        isModified: false,
+      });
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
@@ -77,4 +121,5 @@ module.exports = {
   handleGetAllUsers,
   handleGetUniqueUser,
   handleDeleteUser,
+  handleUpdateUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,13 @@ const {
   handleGetAllUsers,
   handleGetUniqueUser,
   handleDeleteUser,
+  handleUpdateUser,
 } = require('../controller/user');
 
 userRouter.post('/create', handleCreateUser);
 userRouter.get('/', handleGetAllUsers);
 userRouter.get('/:id', handleGetUniqueUser);
 userRouter.delete('/:id', handleDeleteUser);
+userRouter.put('/:id', handleUpdateUser);
 
 module.exports = userRouter;
