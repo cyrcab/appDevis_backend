5ae3dd1e8d270770226ac46e015df7063903cd70
cyril cabrolier <cyr.cab@hotmail.fr>
2022-05-16 14:46:34 +0200

    test route deleted

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
-const testRoute = require('./testRoutes');
 const userRouter = require('./user');
 
 const setupRoutes = (app) => {
-  app.use('/api', testRoute);
   app.use('/api/users', userRouter);
 };
 
