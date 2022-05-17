a1b548620eccf62fc9ead9e0be2690d0398c479e
cyril cabrolier <cyr.cab@hotmail.fr>
2022-05-17 09:55:54 +0200

    controller delete question done

--- controller/question.js
+++ controller/question.js
@@ -45,7 +45,7 @@ async function handleGetAllQuestions(req, res, next) {
             },
           },
         },
-        Category_has_Question: true,
+        Question_has_Response: true,
       },
     });
     res.status(200).json(listOfQuestions);
@@ -97,7 +97,50 @@ async function handleGetUniqueQuestion(req, res, next) {
 }
 async function handleDeleteQuestion(req, res, next) {
   try {
-    
+    const { id } = req.params;
+    const question = await prisma.question.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+      select: {
+        User_id: false,
+        id: true,
+        created_at: true,
+        updated_at: true,
+        content: true,
+        is_public: true,
+        has_multiple_choice: true,
+        indication: true,
+        User: {
+          select: {
+            LastName: true,
+            FirstName: true,
+            mail: true,
+            Role: {
+              select: {
+                Name: true,
+              },
+            },
+          },
+        },
+        Question_has_Response: true,
+      },
+    });
+    if (question) {
+      await prisma.question.delete({
+        where: { id: question.id },
+      });
+      res.status(200).json({
+        message: `question with id : ${id} correctly deleted`,
+        question: { ...question },
+        isDeleted: true,
+      });
+    } else {
+      res.status(404).json({
+        message: `question with id : ${id} does not exist`,
+        isDeleted: false,
+      });
+    }
   } catch (error) {
     console.error(next);
     next(error);
