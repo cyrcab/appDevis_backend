18b9799f49c7e40c709a62e7ce056367f406bb48
cyril cabrolier <cyr.cab@hotmail.fr>
2022-05-18 09:36:14 +0200

    user datas validation done

--- middlewares/validation/user.js
+++ middlewares/validation/user.js
@@ -1,5 +1,54 @@
 const Joi = require('joi');
 
-const userCreateScema = Joi.object({
-  
-})
+const userCreateSchema = Joi.object({
+  firstName: Joi.string().max(45).required(),
+  lastName: Joi.string().max(45).required(),
+  role_id: Joi.number().required(),
+  created_at: Joi.date(),
+  updated_at: Joi.date(),
+  password: Joi.string().max(255).min(6).required(),
+  mail: Joi.string().max(255).email().required(),
+  modified_by: Joi.string(),
+});
+
+const userUpdateSchema = Joi.object({
+  firstName: Joi.string().max(45),
+  lastName: Joi.string().max(45),
+  role_id: Joi.number(),
+  created_at: Joi.date(),
+  updated_at: Joi.date(),
+  password: Joi.string().max(255).min(6),
+  mail: Joi.string().max(255).email(),
+  modified_by: Joi.string(),
+});
+
+const userCreationValidation = (req, res, next) => {
+  const payload = req.body;
+  const { error } = userCreateSchema.validate(payload, {
+    abortEarly: false,
+  });
+  if (error) {
+    next(error);
+    console.error(error);
+  } else {
+    next();
+  }
+};
+
+const userUpdateValidation = (req, res, next) => {
+  const payload = req.body;
+  const { error } = userUpdateSchema.validate(payload, {
+    abortEarly: false,
+  });
+  if (error) {
+    next(error);
+    console.error(error);
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  userCreationValidation,
+  userUpdateValidation,
+};
