a341a7c83869b8500090502ab486d4226157bd11
cyril cabrolier <cyr.cab@hotmail.fr>
2022-07-04 10:55:49 +0200

    delete estimate

--- controller/estimate.js
+++ controller/estimate.js
@@ -1,10 +1,40 @@
 const prisma = require('../helpers/prismaClient');
 const formatDate = require('../helpers/formatDate');
 
+const defaultSelectOption = {
+  id: true,
+  created_at: true,
+  price: true,
+  Category: {
+    select: {
+      name: true,
+    },
+  },
+  Customer: true,
+  type: true,
+  created_by: true,
+  Estimate_has_Answer: {
+    select: {
+      Answer: true,
+    },
+  },
+};
+const userInfo = {
+  select: {
+    lastName: true,
+    firstName: true,
+    mail: true,
+    Role: {
+      select: {
+        Name: true,
+      },
+    },
+  },
+};
+
 async function handleCreateEstimate(req, res, next) {
   try {
     const dateCreation = formatDate(new Date());
-    console.log(req.body);
     const estimateToCreate = await prisma.estimate.create({
       data: {
         ...req.body,
@@ -18,4 +48,52 @@ async function handleCreateEstimate(req, res, next) {
   }
 }
 
-module.exports = { handleCreateEstimate };
+async function handleGetAllEstimate(req, res, next) {
+  try {
+    const listOfEstimates = await prisma.estimate.findMany({
+      select: {
+        ...defaultSelectOption,
+        User: { ...userInfo },
+      },
+      take: 10,
+    });
+    if (listOfEstimates.length) {
+      res.status(200).json(listOfEstimates);
+    } else {
+      res.status(404).json({ message: 'no estimate found' });
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+async function handleDeleteEstimate(req, res, next) {
+  try {
+    const { id } = req.params;
+    const estimate = await prisma.estimate.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    if (estimate) {
+      const estimateToDelete = await prisma.estimate.delete({
+        where: {
+          id: parseInt(id),
+        },
+      });
+      if (estimateToDelete) {
+        res.status(200).json({ message: 'estimate deleted' });
+      } else {
+        res.status(400).json({ message: 'error when deleting estimate' });
+      }
+    } else {
+      res.status(404).json({ message: 'estimate not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
+module.exports = { handleCreateEstimate, handleGetAllEstimate, handleDeleteEstimate };
--- routes/estimate.js
+++ routes/estimate.js
@@ -1,6 +1,12 @@
 const estimateRouter = require('express').Router();
-const { handleCreateEstimate } = require('../controller/estimate');
+const {
+  handleCreateEstimate,
+  handleGetAllEstimate,
+  handleDeleteEstimate,
+} = require('../controller/estimate');
 
 estimateRouter.post('/', handleCreateEstimate);
+estimateRouter.get('/', handleGetAllEstimate);
+estimateRouter.delete('/:id', handleDeleteEstimate);
 
 module.exports = estimateRouter;
