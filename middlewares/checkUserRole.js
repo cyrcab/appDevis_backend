3597f43d493c685a6d8a0c3074faa3595e9bf594
cyril cabrolier <cyr.cab@hotmail.fr>
2022-07-12 18:14:18 +0200

    fix bug checking role

--- middlewares/checkUserRole.js
+++ middlewares/checkUserRole.js
@@ -1,7 +1,5 @@
-const checkUserRole = async (req, res, next) => {
-  console.log(req.user);
-  
-  if (!req.user.role_id === 1) {
+const checkUserRole = (req, res, next) => {
+  if (req.user.role_id !== 1) {
     return res.status(401).send({ message: "Vous n'avez pas l'autorisation" });
   }
   next();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import checkUserRole from '../middlewares/checkUserRole';
 import userRouter from './user.router';
 import packRouter from './pack.router';
-// import questionRouter from './question.js';
+
 import answerRouter from './option.router';
 // import customerRouter from './customer';
 // import estimateRouter from './estimate';
@@ -12,7 +12,7 @@ const setupRoutes = (app) => {
   app.use('/api/users', userRouter);
   app.use('/api/packs', [checkUserRole, packRouter]);
   // app.use('/api/questions', questionRouter);
-  app.use('/api/options', answerRouter);
+  app.use('/api/options', [checkUserRole, answerRouter]);
   // app.use('/api/customer', customerRouter);
   // app.use('/api/estimates', estimateRouter);
 };
