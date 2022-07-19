5ad8c542edb54d7b5e557d413d5c65f6499fa09e
cyril cabrolier <cyr.cab@hotmail.fr>
2022-07-19 22:33:49 +0200

    customer route + controller updating with es6

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ import userRouter from './user.router';
 import packRouter from './pack.router';
 
 import answerRouter from './option.router';
-// import customerRouter from './customer';
+import customerRouter from './customer.router';
 import fileRouter from './file.router';
 
 const setupRoutes = (app) => {
   app.use('/api/users', userRouter);
   app.use('/api/packs', packRouter);
   app.use('/api/options', answerRouter);
-  // app.use('/api/customer', customerRouter);
+  app.use('/api/customer', customerRouter);
   app.use('/api/files', fileRouter);
 };
 
