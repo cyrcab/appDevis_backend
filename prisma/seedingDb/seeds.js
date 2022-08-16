2ba73e86e6b227abe306d1b79dce2bdfcf672df6
cyril cabrolier <cyr.cab@hotmail.fr>
2022-08-16 14:23:57 +0200

    change seeding method

--- prisma/seedingDb/seeds.js
+++ prisma/seedingDb/seeds.js
@@ -1,3 +1,5 @@
+const { SUPERADMIN_PASS, SUPERADMIN_MAIL } = process.env;
+
 const roles = [
   {
     Name: 'Admin',
@@ -5,9 +7,8 @@ const roles = [
 ];
 const users = [
   {
-    mail: 'admin@mail.fr',
-    password:
-      '$argon2id$v=19$m=65536,t=5,p=1$5fHcVfQYCTVaKSWrA6LzUA$M4POlUs1M+EF6pXaCbWt0hZEW8VOKezwFeiHtuDeYI4',
+    mail: SUPERADMIN_MAIL,
+    password: SUPERADMIN_PASS,
     firstName: 'admin',
     lastName: 'admin',
     role_id: 1,
