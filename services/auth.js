00d39e13e5c8c19a78bb8428e8b81ad5ed7172ad
cyril cabrolier <cyr.cab@hotmail.fr>
2022-08-17 18:45:43 +0200

    check errors in protect middleware

--- services/auth.js
+++ services/auth.js
@@ -112,37 +112,38 @@ export const signin = async (req, res, next) => {
 export const protect = async (req, res, next) => {
   const bearer = req.headers.authorization;
 
-  if (!bearer || !bearer.startsWith('Bearer ')) {
-    return res.status(401).send('Pas de bearer détecté');
-  }
-
-  const token = bearer.split('Bearer ')[1].trim();
-  let payload;
-
   try {
-    payload = await verifyToken(token);
+    if (!bearer || !bearer.startsWith('Bearer ')) {
+      return res.status(401).send('Pas de bearer détecté');
+    }
+
+    const token = bearer.split('Bearer ')[1].trim();
+    let payload;
+
+    try {
+      payload = await verifyToken(token);
+    } catch (error) {
+      next(error);
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: payload.id,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).send('Invalid user');
+    }
+
+    delete user.password;
+
+    req.user = user;
+    next();
   } catch (error) {
     next(error);
+    return res.status(500).end();
   }
-
-  if (!payload) {
-    return res.status(401).send('Invalid user');
-  }
-
-  const user = await prisma.user.findUnique({
-    where: {
-      id: payload.id,
-    },
-  });
-
-  if (!user) {
-    return res.status(401).send('Invalid user');
-  }
-
-  delete user.password;
-
-  req.user = user;
-  next();
 };
 
 export const checkUserRole = (req, res, next) => {
